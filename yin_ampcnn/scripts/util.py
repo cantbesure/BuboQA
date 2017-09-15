273404837ba06bd51ee68e18dbeca22d4320cbf4
Salman Mohammed <smohammed1993@gmail.com>
2017-09-15 14:25:55 -0400

    added dir for yins model

---
+++ yin_ampcnn/scripts/util.py
@@ -0,0 +1,24 @@
+import unicodedata
+from nltk.tokenize.treebank import TreebankWordTokenizer
+
+tokenizer = TreebankWordTokenizer()
+
+def processed_text(text):
+    stripped = strip_accents(text.lower())
+    toks = tokenizer.tokenize(stripped)
+    return " ".join(toks)
+
+def strip_accents(text):
+    return ''.join(c for c in unicodedata.normalize('NFKD', text) if unicodedata.category(c) != 'Mn')
+
+def www2fb(in_str):
+    if in_str.startswith("www.freebase.com"):
+        return 'fb:%s' % (in_str.split('www.freebase.com/')[-1].replace('/', '.'))
+    return in_str
+
+def clean_uri(uri):
+    if uri.startswith("<") and uri.endswith(">"):
+        return clean_uri(uri[1:-1])
+    elif uri.startswith("\"") and uri.endswith("\""):
+        return clean_uri(uri[1:-1])
+    return uri
